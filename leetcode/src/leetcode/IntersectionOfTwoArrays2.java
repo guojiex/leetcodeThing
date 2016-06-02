9563f3b3e97eee839111a4373bb9ebabf576d5a2
Jiexin Guo <1036479561@qq.com>
2016-06-02 17:25:28 +0800

    add python ex

--- leetcode/src/leetcode/IntersectionOfTwoArrays2.java
+++ leetcode/src/leetcode/IntersectionOfTwoArrays2.java
@@ -11,18 +11,22 @@ import java.util.List;
 Given two arrays, write a function to compute their intersection.
 
 Example:
-Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2].
+Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2, 2].
 
 Note:
-Each element in the result must be unique.
+Each element in the result should appear as many times as it shows in both arrays.
 The result can be in any order.
+Follow up:
+What if the given array is already sorted? How would you optimize your algorithm?
+What if nums1's size is small compared to num2's size? Which algorithm is better?
+What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
 */
 /**
  * 这道题的目标是找出两个整数数组公共的部分（而且要去重，就是如果重复了两个2，只输出一个2）
  * 我的思路是用一个bit vector当做map用，先把第一个数组的所有数字遍历一遍存入map,然后遍历第二个数组查看公共部分
  * @author Jiexin Guo
  */
-public class IntersectionOfTwoArrays {
+public class IntersectionOfTwoArrays2 {
     public static int[] intersection(int[] nums1, int[] nums2) {
         List<Integer> result = new ArrayList<>();
         int[] map = new int[1000];
@@ -37,7 +41,8 @@ public class IntersectionOfTwoArrays {
             boolean exist=((map[index]>>rightShift)&0x01)==1;
             if(exist){
                 result.add(i);
-                map[index]&=(~(1<<rightShift));
+                continue;
+                //map[index]&=(~(1<<rightShift));
             }
         }
         int[] newRes=new int[result.size()];
@@ -52,8 +57,8 @@ public class IntersectionOfTwoArrays {
      * @param args
      */
     public static void main(String[] args) {
-        int[] num1 = { 1, 2, 2, 1 };
-        int[] num2 = { 2, 2 };
+        int[] num1 = {1};// 1, 2, 2, 1 };
+        int[] num2 = {1,1};// 2, 2 };
         int[] result=intersection(num1,num2);
         for(int i:result){
             System.out.println(i);
---
+++ pythonExercise/ex2.py
@@ -0,0 +1,9 @@
+type("Hello, World!")
+type(17)
+type('This is a string.')
+
+print('''"Oh no", she exclaimed, "Ben's bike is broken!"''')
+
+message = "What's up, Doc?"
+n = 17
+pi = 3.14159
